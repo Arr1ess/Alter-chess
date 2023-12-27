0112b68f9483e0191bdb5056347c5dcbcc1a5aef
Arr1ess <146137707+Arr1ess@users.noreply.github.com>
2023-12-27 15:03:51 +0300

    Delete public directory

--- public/php/errorOutput.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['success'])) {
-	echo "<div class=\"alert alert-success\" role=\"alert\">" . $_GET['success'] . "</div>";
-} elseif (isset($_GET['error'])) {
-	echo "<div class=\"alert alert-danger\" role=\"alert\">" . $_GET['error'] . "</div>";
-}
-?>
