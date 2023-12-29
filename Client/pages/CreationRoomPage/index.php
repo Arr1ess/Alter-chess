1ecf914e0cc5f7613009467730e00ac9b80657c5
Arr1ess <146137707+Arr1ess@users.noreply.github.com>
2023-12-29 11:32:04 +0300

    Объединил две ветки, возможно криво
    Изменил структуру проекта, включил в него scss

---
+++ Client/pages/CreationRoomPage/index.php
@@ -0,0 +1,61 @@
+<?php
+$gameName = $_POST['gameName'] ?? '';
+function generateGuid()
+{
+	return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
+		mt_rand(0, 65535),
+		mt_rand(0, 65535),
+		mt_rand(0, 65535),
+		mt_rand(16384, 20479),
+		mt_rand(32768, 49151),
+		mt_rand(0, 65535),
+		mt_rand(0, 65535),
+		mt_rand(0, 65535)
+	);
+}
+
+$gameId = generateGuid();
+$minutes = 0;
+$seconds = 0;
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Создание комнаты</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<main class="wrapper">
+		<h2>Cоздать
+			<? $gameName ?>
+		</h2>
+		<form class="card">
+			<div class="labels">
+				<label for="initialTime">Время на партию <br /> (мин):</label>
+				<label for="addedTime">Добавление <br />(сек):</label>
+			</div>
+			<div class="inputs">
+				<input type="number" id="initialTime" name="initialTime" step="1" min="0" value=<?= $minutes?>>
+				<input type="number" id="addedTime" name="addedTime" step="1" min="0" value=<?= $seconds?>>
+			</div>
+			<div class="button-wrapper">
+				<input type="submit" class="btn outline" value="Добавить партию">
+			</div>
+		</form>
+
+
+		<script>
+		gameId = '<?= $gameId ?>';
+		gameName = '<?= $gameName ?>';
+		</script>
+		<script src="scripts/wsConection.js"></script>
+
+	</main>
+</body>
+
+</html>
